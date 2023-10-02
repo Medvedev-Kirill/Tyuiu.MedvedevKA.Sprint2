19d1385b5f2a334b61cf462e413341957ff92f62
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-10-02 23:52:00 +0500

    Task1

---
+++ Tyuiu.MedvedevKA.Sprint2.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+// ЗАДАНИЕ
+// Написать программу из операций сравнений (==, !=, <, >, <=, >=,
+// последовательность можно чередовать, но использовать один раз в выражении)
+// и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться)
+// и а также арифметических выражений, которая вернет логическую последовательность(массив): (False, False, False, False, True, False),
+// при a = 696, b = 987, c = 696, d = 155
+
+namespace Tyuiu.MedvedevKA.Sprint2.Task1.V21.Lib
+{
+    public class DataService : ISprint2Task1V21
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c < d);
+            res[1] = (a + 5 > b) & (c < d);
+            res[2] = (a > b) || (c < d);
+            res[3] = (a + 5 > b) && (c < d);
+            res[4] = !res[0];
+            res[5] = (a > b) ^ (c < d);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint2.Task1.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevKA.Sprint2.Task1.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevKA.Sprint2.Task1.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86b9d745-15d2-4918-9177-fe458af33261")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
