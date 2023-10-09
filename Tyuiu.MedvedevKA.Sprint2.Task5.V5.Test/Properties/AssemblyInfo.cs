7601e572822ebed1f4312f68f7d3edf4e21339a7
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-10-09 20:52:29 +0500

    Task5

---
+++ Tyuiu.MedvedevKA.Sprint2.Task5.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevKA.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevKA.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49ac0401-5da5-42e1-bcf6-57f748b59a08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
