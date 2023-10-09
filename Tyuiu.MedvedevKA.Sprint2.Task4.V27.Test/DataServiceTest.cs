1eae277055bc02969efff4f81686dc20f6b3d023
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-10-09 12:21:26 +0500

    Task 4

---
+++ Tyuiu.MedvedevKA.Sprint2.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MedvedevKA.Sprint2.Task4.V27.Lib;
+
+namespace Tyuiu.MedvedevKA.Sprint2.Task4.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint2.Task4.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevKA.Sprint2.Task4.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevKA.Sprint2.Task4.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35cfcd6b-6bdb-43c4-a457-b0255a8963da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
